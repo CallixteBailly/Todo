d885b88d4fcabb6a8430210eabb7a394833a9f13
bailly.nthony <bailly.nthony@gmail.com>
2023-01-01 01:43:15 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 function App() {
+  const lastName = "P-A"
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>Hello {value} {lastName}</div>;
 }
 
 export default App;
