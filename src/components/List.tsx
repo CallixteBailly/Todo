585c759d78ffc11a948738746b4ee451df237713
BAILLY Tremanirina <tremanirina.bailly@fnacdarty.com>
2023-01-08 23:12:47 +0100

    feat(submit)Add a text field and a button to add a new task to the list (using forms and refs)

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,12 +1,26 @@
+import React, { useState, useRef } from 'react';
 import Task from './Task';
 
 function List(props: { tasks: any[]; }) {
+    const [tasks, setTasks] = useState(props.tasks);
+    const [newTask, setNewTask] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        setTasks([...tasks, newTask]);
+        setNewTask('');
+    }
+
     return (
         <><p>To-do List:</p><ul>
-            {props.tasks.map((item: any, index: any) => (
+            {tasks.map((item: any, index: any) => (
                 <Task key={index} item={item} />
             ))}
-        </ul></>);
+        </ul>
+        <form onSubmit={handleSubmit}>
+        <input value={newTask} onChange={event => setNewTask(event.target.value)} />
+        <button type="submit">Add task</button>
+      </form></>);
 }
 
 export default List;
