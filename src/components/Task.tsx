091d28e38357180018c7982491be172ce2f54d6a
BAILLY Tremanirina <tremanirina.bailly@fnacdarty.com>
2023-01-08 19:26:16 +0100

    feat(state) Manage the state of each task (completed or not completed) with the state of the TodoItem component

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Task(props) {
-    return (<li>{props.item}</li>);
+    const [isCompleted, setIsCompleted] = useState(false);
+
+    function handleClick() {
+        setIsCompleted(prevState => !prevState);
+      }
+    
+      return (
+        <li onClick={handleClick} style={{ textDecoration: isCompleted ? 'line-through' : 'none' }}>
+          {props.item}
+        </li>)
 }
 
 export default Task;
